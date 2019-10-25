3523bb219acd53c0593d94e75734b547f7746f7c
Tonia Seenauth <tonia.seenauth@homes.com>
2019-10-25 15:44:53 -0400

    adding header component

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const practiceheader = () => {
+    return(
+        <h1>Basic Git Commands - Quick Guide</h1>
+    )
+}
+
+export {practiceheader};
---
+++ src/components/main-body.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const practicemainbody = () => {
+    return (
+        <div></div>
+    )
+}
